543eac6fcc76339547a3eb617aba959ff9b21631
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-13 14:13:04 +0100

    commit

--- Data_uploading.py
+++ Data_uploading.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-engine=create_engine("postgresql+psycopg2://postgres:xxxxxxxx@localhost:5432/postgres")
-df = pd.read_csv('path_to_.csv_file')
+engine=create_engine("postgresql+psycopg2://postgres:new@1610@localhost:5432/jobs")
+df = pd.read_csv('team.csv')
 
 try:
-    df.to_sql('table_name', engine, if_exists= 'replace', index= False)
+    df.to_sql('team', engine, if_exists= 'replace', index= False)
 
 except:
     print("Sorry, some error has occurred!")
