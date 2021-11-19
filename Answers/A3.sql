8b6a03568254107fe0fecec2c14835cc5aaaf751
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-19 04:00:19 +0100

    commit

---
+++ Answers/A3.sql
@@ -0,0 +1,8 @@
+Select con.conv_date,conb.conv_date,
+conb.user_id,conb.conv_id,conb.revenue,conb.market
+from conversions con
+right join conversions_backend conb
+on conb.conv_id = con.conv_id
+and con.conv_date is null ;
+
+/* conversions table conv_date field has null values */
